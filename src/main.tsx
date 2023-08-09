2201a72fa9a6dbd49d3b18c38030433badcf3831
Minhnq24751 <mn9175719@gmail.com>
2023-08-09 14:44:26 +0700

    udpate-base

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import App from "./App.tsx";
-import "./index.css";
-import persistor, { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <Provider store={store}>
-        <PersistGate loading="null" persistor={persistor}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
